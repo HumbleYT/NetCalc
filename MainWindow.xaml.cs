699591229afdf7b2728b0f93230ef250555bc4d2
Eldon <ELDON@ESPC>
2026-06-24 00:52:26 +0100

    Adapted the calculation to now include modulus and remainder ensuring that there is a different sign for each.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -42,25 +42,36 @@ namespace NetCalc
                 {
                     if (arr[i] == "*")
                     {
-                        arr[i - 1] = (Convert.ToInt32(arr[i - 1]) * Convert.ToInt32(arr[i + 1])).ToString();
+                        arr[i - 1] = (Convert.ToDouble(arr[i - 1]) * Convert.ToDouble(arr[i + 1])).ToString();
                         arr = arr.Where((source, index) => index != i && index != i + 1).ToArray();
                     }
                     else if (arr[i] == "/")
                     {
-                        arr[i - 1] = (Convert.ToInt32(arr[i - 1]) / Convert.ToInt32(arr[i + 1])).ToString();
+                        arr[i - 1] = (Convert.ToDouble(arr[i - 1]) / Convert.ToDouble(arr[i + 1])).ToString();
+                        arr = arr.Where((source, index) => index != i && index != i + 1).ToArray();
+                    }
+                    else if (arr[i] == "%")
+                    {
+                        arr[i - 1] = (Convert.ToDouble(arr[i - 1]) % Convert.ToDouble(arr[i + 1])).ToString();
+                        arr = arr.Where((source, index) => index != i && index != i + 1).ToArray();
+                    }
+                    else if (arr[i] == "~")
+                    {
+                        arr[i - 1] = (Convert.ToInt32(arr[i - 1]) % Convert.ToInt32(arr[i + 1])).ToString();
                         arr = arr.Where((source, index) => index != i && index != i + 1).ToArray();
                     }
                 }
+            
                 for (int i = 0; i < arr.Length; i++)
                 {
                     if (arr[i] == "+")
                     {
-                        arr[i - 1] = (Convert.ToInt32(arr[i - 1]) + Convert.ToInt32(arr[i + 1])).ToString();
+                        arr[i - 1] = (Convert.ToDouble(arr[i - 1]) + Convert.ToDouble(arr[i + 1])).ToString();
                         arr = arr.Where((source, index) => index != i && index != i + 1).ToArray();
                     }
                     else if (arr[i] == "-")
                     {
-                        arr[i - 1] = (Convert.ToInt32(arr[i - 1]) - Convert.ToInt32(arr[i + 1])).ToString();
+                        arr[i - 1] = (Convert.ToDouble(arr[i - 1]) - Convert.ToDouble(arr[i + 1])).ToString();
                         arr = arr.Where((source, index) => index != i && index != i + 1).ToArray();
                     }
                 }
@@ -85,13 +96,93 @@ namespace NetCalc
 
         private void subBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            answerBox.Text += "-";
         }
 
         private void eqlBtn_Click(object sender, RoutedEventArgs e)
         {
             string x = calculate(answerBox.Text);
             answerBox.Text = x;
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "0";
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "3";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "4";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "6";
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "7";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "8";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "9";
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "+";
+        }
+
+        private void multBtn_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "*";
+        }
+
+        private void divBtn_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "/";
+        }
+
+        private void modBtn_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "%";
+        }
+
+        private void remBtn_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Text += "~";
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
